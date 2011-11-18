9ca96200f3eacdfe74009d5426d20c4966d804fa
Cliff Maury <cliff.maury@gmail.com>
2011-11-18 20:58:58 +0100

    Initial commit for submodule puppet-sonar from Geppeto

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'cliff-puppet-sonar'
+version '0.0.1'
+source ''
+author 'cliff'
+license ''
+summary ''
+description ''
+project_page ''
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: puppet-sonar
+#
+# This module manages puppet-sonar
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-sonar {
+
+
+}
